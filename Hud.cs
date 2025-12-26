d8b5e5c38e5f3b9f85b368a2064b5f9e0a57238c
jonp382 <290140399+jonp382@users.noreply.github.com>
2025-12-26 01:24:11 -0500

    Added targetting info incl. object name, mass and velocity

--- Hud.cs
+++ Hud.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public partial class Hud : CanvasLayer
@@ -8,11 +9,15 @@ public partial class Hud : CanvasLayer
 	[Signal]
 	public delegate void StartGameEventHandler();
 
+	[Export] public uint HoverCollisionLayer = 1;
+
 	private Player player;
 
 	private int compass_tick = 0;
 
 	private Planet ClosestPlanet;
+
+	private Node2D CurrentHovered = null;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -27,12 +32,59 @@ public partial class Hud : CanvasLayer
 		compass_tick++;
 		UpdateCompass();
 		UpdateSpeed();
-
+		UpdateTarget();
+		
+		
 	}
+
+	public Node2D GetMouseoverNode()
+	{
+		var Viewport = GetViewport();
+		var SpaceState = PhysicsServer2D.SpaceGetDirectState(Viewport.World2D.Space);
+		var MousePos_Viewport = Viewport.GetMousePosition();
+		var Camera = Viewport.GetCamera2D();
+
+		Vector2 MousePos_Global = MousePos_Viewport;
+
+		if (Camera != null)
+		{
+			// MousePos_Global = Camera.GetGlobalTransform().AffineInverse() * MousePos_Viewport; // WTF DOES THIS DO?
+			MousePos_Global = Viewport.CanvasTransform.AffineInverse() * MousePos_Viewport;
+		}
+
+		// if no camera, viewport mouse pos == global mouse pos.
+
+		var query = new PhysicsPointQueryParameters2D
+		{
+			Position = MousePos_Global,
+			CollisionMask = 1u << ((int)HoverCollisionLayer - 1), // WTF DOES THIS DO
+			CollideWithAreas = true,
+			CollideWithBodies = true,
+		};
+
+		var hits = SpaceState.IntersectPoint(query, 4);
+		// GD.Print($"Global Pos: {MousePos_Global} -- Planet Pos: {ClosestPlanet.Position} -- Player Pos: {player.Position} -- Hits: {hits.Count}");
+
+
+		Node2D newHovered = null;
+
+		if (hits.Count > 0)
+		{
+			var colliderObj = hits[0]["collider"].Obj;
+			if(colliderObj is Node2D collider)
+			{
+				newHovered = collider;
+			}
+		}
+
+		return newHovered;
+		
+	}
+
 
 	public void UpdateCompass()
 	{
-		if(compass_tick % 10 == 0 || ClosestPlanet == null) ClosestPlanet = FindClosestPlanet();
+		if(compass_tick % 40 == 0 || ClosestPlanet == null) ClosestPlanet = FindClosestPlanet();
 
 		float angle = 0f;
 
@@ -86,6 +138,36 @@ public partial class Hud : CanvasLayer
 		var messageNode = GetNode<Label>("CurrentSpeed");
 		messageNode.Text = text;
 		messageNode.Show();
+	}
+
+	public void UpdateTarget()
+	{
+		Node2D result = GetMouseoverNode(); // returns global CurrentHovered node.
+		if(result is RigidBody2D) CurrentHovered = result;
+
+		if(CurrentHovered == null) return;
+
+		Label label = GetNode<Label>("Target");
+		List<String> TextList = [];
+
+		try
+		{
+			TextList.Add($"Target: {CurrentHovered.Name}");
+			if(CurrentHovered is RigidBody2D Body) 
+			{
+				TextList.Add($"Mass: {Body.Mass:F0}");
+				TextList.Add($"Vel:  {Body.LinearVelocity.Length():F0}");
+			}
+
+			label.Text = String.Join("\n", TextList);
+		}
+		catch
+		{
+			label.Text = "Target: Object was deleted";
+		}
+		
+		
+
 	}
 
 }
